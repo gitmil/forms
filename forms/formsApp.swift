f5b2a130aed889c30f1b149e0e3ba886cf018572
hemil patel <gentindian@hotmail.com>
2022-02-19 09:16:20 -0800

    Initial Commit

---
+++ forms/formsApp.swift
@@ -0,0 +1,17 @@
+//
+//  formsApp.swift
+//  forms
+//
+//  Created by Hemil Patel on 2/19/22.
+//
+
+import SwiftUI
+
+@main
+struct formsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
